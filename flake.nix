4f8bd41ead4a92a8d28845ea2aabea291c0711c9
Seu Nome <seu@email.com>
2026-04-01 08:35:36 -0300

    Comitado

--- flake.nix
+++ flake.nix
@@ -1,35 +1,32 @@
-{ description = "Ambiente dev web - Carlos";
+{
+  description = "Ambiente de desenvolvimento Web - Carlos";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux"; 
+      system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      
-      node22 = pkgs.nodejs_22.overrideAttrs (old: rec {
-        version = "22.17.0";
-        src = pkgs.fetchurl {
-          url = "https://nodejs.org/dist/v22.17.0/node-v22.17.0.tar.xz";
-          sha256 = "7a3ef2aedb905ea7926e5209157266e2376a5db619d9ac0cba3c967f6f5db4f9";
-        };
-      });
     in {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          node22
-          docker
-          docker-compose
-          insomnia
-          dbeaver-bin
+        buildInputs = [
+          pkgs.nodejs_22
+
+    
+
+          pkgs.docker
+          pkgs.docker-compose
+
+          pkgs.insomnia
+          pkgs.dbeaver-bin
         ];
 
         shellHook = ''
-          echo "Ambiente carregado"
-          node -v
-          npm -v
+          echo "Ambiente de Desenvolvimento Web carregado!"
+          echo "Node: $(node -v)"
+          echo "NPM: $(npm -v)"
         '';
       };
     };
